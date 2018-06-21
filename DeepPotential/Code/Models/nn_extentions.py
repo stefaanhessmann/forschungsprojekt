7e9769aaba17a5ef3e4b61f1c89d9ab4e4e957f1
Stefaan Hessmann <hessmann.stefaan@googlemail.com>
2018-06-21 14:44:28 +0200

    clean up

--- DeepPotential/Code/Models/nn_extentions.py
+++ DeepPotential/Code/Models/nn_extentions.py
@@ -2,12 +2,12 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 class AbcExponentialLR(_LRScheduler):
 
-    def __init__(self, optimizer, a, b, c, last_epoch=-1):
+    def __init__(self, optimizer, b, c, last_epoch=-1):
         #self.a = a
         self.b = b
         self.c = c
-        super(ExponentialLR, self).__init__(optimizer, last_epoch)
+        super().__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        return [base_lr * self.b ** (0.6*self.last_epoch/c)
+        return [base_lr * self.b ** (0.6*self.last_epoch/self.c)
                 for base_lr in self.base_lrs]
